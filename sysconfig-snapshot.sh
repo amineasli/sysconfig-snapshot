0d861b3c342960725751278a38aa4fd171c43c20
AmineAsli <asli.amine@gmail.com>
2015-07-28 15:34:45 -0400

    add harware info functions + command line usage

--- sysconfig-snapshot.sh
+++ sysconfig-snapshot.sh
@@ -2,13 +2,22 @@
 #
 # Platform : Linux
 #
-# Purpose : Capture the state of the system configuration for later troubleshooting in the event of system problems. The given data will be stored in the file defined in $SYSCONFIGFILE variable.
+# Purpose : Capture the state of the system configuration for later
+# troubleshooting in the event of system problems. The given data will be stored
+# in the file defined in $SYSCONFIGFILE variable.
 #
 # Usage :
-#         sysconfig-snapshot.sh 
 
-PROGRAM=`basename $0`
+#       sysconfig-snapshot.sh [--output file] [--verbose] [--quiet] [--help] \
+#                             [--version]
+                                
+
+
+PROGRAM=$(basename $0)
 VERSION=0.1
+CURRENTHOST=$(hostname)
+DATETIME=$(date +%m%d%y_%H%M%S)
+SYSCONFIGFILE=sysconfig-snapshot.$CURRENTHOST.$DATETIME
 
 error()
 {
@@ -18,7 +27,9 @@ error()
 
 usage()
 {
-   echo "Usage: $PROGRAM"
+   echo "Usage: $PROGRAM [--output file] [--verbose] [--quiet] [--help] \
+         [--version]"
+
 }
 
 usage_and_exit()
@@ -31,3 +42,60 @@ version()
 {
    echo "$PROGRAM version $VERSION"
 }
+
+get_os()
+{
+   uname -mrs
+}
+
+get_timezone()
+{
+   date +'%z %Z'
+}
+
+get_cpu_info()
+{
+   lspci
+}
+
+get_real_mem()
+{
+   free -h | grep Mem | awk '{ printf("Total : %s\nUsed : %s\nFree : %s\n"\
+, $2, $3, $4)}'
+}
+
+get_sys_hw_info()
+{
+   dmidecode -q
+}
+
+get_pci_list()
+{
+   lspci -m
+}
+
+ 
+while test $# -gt 0 
+do 
+   case $1 in
+   --output | -o )
+     SYSCONFIGFILE=$1
+     ;;
+   --help | -h )
+      usage_and_exit 0
+      ;;
+   --version | -v )
+      version
+      exit 0
+      ;;
+   -*) 
+      error "Unrecognized option: $1"
+      ;;
+   *)
+      break
+      ;;
+   esac
+   shift
+done
+
+
